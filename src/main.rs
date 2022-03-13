3dc23d222cb4d466ed02ab30802d5d1c2dd1dd2b
gatlace <loginemail300@gmail.com>
2022-03-12 21:57:16 -0500

    Changed blackjack to have only one player

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 #![allow(dead_code)]
+mod base;
 mod games;
 mod generators;
-mod base;
 
 use games::blackjack;
 
 fn main() {
     blackjack::do_game();
 }
+
+
+//TODO:
+// - save player data with a csv file
+// - craps
+// - GUI for all of this
+// - I might have to rig it, I got a $1500 bank in 5 rounds
