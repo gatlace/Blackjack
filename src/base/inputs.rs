fe4107182904027a406cacadb3d061f87bdbbb2d
gatlace <loginemail300@gmail.com>
2022-03-12 22:42:10 -0500

    i merged main to dev by mistake

---
+++ src/base/inputs.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+pub fn get_string(prompt: &str) -> String {
+    println!("{}", prompt);
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read string\n");
+
+    input.trim().into()
+}
+
+pub fn get_usize(prompt: &str) -> usize {
+    let mut input: String;
+
+    loop {
+        input = get_string(prompt);
+        break match input.trim().parse::<usize>() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Failed to parse input\n");
+                continue;
+            }
+        };
+    }
+}
+
+pub fn get_bool(prompt: &str) -> bool {
+    loop {
+        match get_string(prompt).to_lowercase().as_str() {
+            "y" | "yes" => break true,
+            "n" | "no" => break false,
+            _ => {
+                println!("Error: y/es or n/o");
+                continue;
+            }
+        }
+    }
+}
